e52cecf24cde42b05fe28476509154408a7dec6a
ChrisDill <chrisdude134@gmail.com>
2018-02-07 11:38:57 +0000

    Added Excel workbook loading
    Loads a excel workbook called test.xlsx or creates a new one.

--- TollTrack/Form1.cs
+++ TollTrack/Form1.cs
@@ -8,16 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.IO;
 
 namespace TollTrack
 {
     public partial class Form1 : Form
     {
         private string TollURL = @"https://online.toll.com.au/trackandtrace/";
-        private SortedList<string,Tuple<string,DateTime>> consignmentIds = new SortedList<string,Tuple<string,DateTime>>() {{"AREW065066",("Unknown",DateTime.MinValue)}}; // ID, Status
+        //private SortedList<string,Tuple<string,DateTime>> consignmentIds = new SortedList<string,Tuple<string,DateTime>>() {{"AREW065066",("Unknown",DateTime.MinValue)}}; // ID, Status
         public Form1()
         {
             InitializeComponent();
+            ExcelTest("test.xlsx");
         }
 
         private void btnRun_Click(object sender, EventArgs e)
@@ -25,27 +28,46 @@ namespace TollTrack
             webBrowser.Navigate(TollURL);
         }
 
-        private void ExcelTest()
+        private void ExcelTest(string filename)
         {
+            // open excel app
             var xl = new Excel.Application();
             if (xl == null)
             {
-                MessageBox.Show("Excel is not properly installed!!");
+                MessageBox.Show("Excel is not installed");
                 return;
             }
-            xl.Visible = true;
+            xl.SheetsInNewWorkbook = 1;
+            xl.Visible = false;
 
-            var workbook = xl.Workbooks.Open("test");
+            // open workbook or create a new ones
+            Excel.Workbook workbook;
+            filename = Path.GetFullPath(filename);
+            if (File.Exists(filename))
+            {
+                workbook = xl.Workbooks.Open(filename);
+            }
+            else
+            {
+                workbook = xl.Workbooks.Add(Missing.Value);
+                workbook.SaveAs(filename);
+            }
+
             var worksheet = workbook.Worksheets[1];
+            worksheet.Cells[1,1] = "test";
+            worksheet.Cells[2, 1] = "space";
+            worksheet.Cells[3, 1] = "things";
 
-            workbook.Save();
+            // save and quit
+            workbook.Close(true, Missing.Value, Missing.Value);
+            xl.Quit();
         }
 
         private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             var trackingIds = "";
             
-            consignmentIds.ForEach(c=> trackingIds += $"{c}{Environment.NewLine}");
+            //consignmentIds.ForEach(c=> trackingIds += $"{c}{Environment.NewLine}");
 
             var command = $"document.getElementById('connoteIds').innerText = '{trackingIds}'; $('.dijitButtonNode').click() ";
 
